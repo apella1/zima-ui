916886c79f1edf4ed77ba2c4fc4e044db9eb370b
apella1 <johncollons5@gmail.com>
2023-03-12 10:41:54 +0300

    Update hero section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,9 @@ export {};
 export default function Header() {
   return (
     <header className="flex flex-col h-screen p-4 m-4" id="home">
-      <div className="selfcenter">
-        <h2 className="text-5xl">Explore The Power Within.</h2>
-        <p className="text-3xl">It's time to be truly you!</p>
+      <div className="mt-16 space-y-4">
+        <h2 className="text-7xl">Find support and guidance </h2>
+        <p className="text-3xl">from a community of people who understand</p>
       </div>
     </header>
   );
