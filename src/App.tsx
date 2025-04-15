4f0d3c36d2b1ed72c8011751e1652583425a176d
John Apella <johncollons5@gmail.com>
2025-04-15 17:09:08 +0300

    feat: update page imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,64 @@
+import RootLayout from "@/layouts/root-layout";
+import Chat from "@/pages/chat/chat";
+import Community from "@/pages/community/community";
+import Conditions from "@/pages/conditions/conditions-page";
+import FAQ from "@/pages/faq/faq-page";
+import Home from "@/pages/home/home";
+import Login from "@/pages/login/login";
+import PrivacyPolicy from "@/pages/privacy/privacy-policy-page";
+import Signup from "@/pages/signup/signup";
+import Terms from "@/pages/terms/terms-page";
+import Therapists from "@/pages/therapist/therapists";
 import { HelmetProvider } from "react-helmet-async";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./App.css";
-import RootLayout from "./layouts/root-layout";
-import Chat from "./pages/chat/chat";
-import Community from "./pages/community/community";
-import ConditionsPage from "./pages/conditions/conditions-page";
 import ErrorPage from "./pages/error/error-page";
-import FAQPage from "./pages/faq/faq-page";
-import Home from "./pages/home/home";
-import Login from "./pages/login/login";
-import PrivacyPolicyPage from "./pages/privacy/privacy-policy-page";
-import Signup from "./pages/signup/signup";
-import TermsPage from "./pages/terms/terms-page";
-import Therapist from "./pages/therapist/therapists";
 
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <Home />, errorElement: <ErrorPage /> },
-      { path: "/login", element: <Login /> },
-      { path: "/signup", element: <Signup /> },
-      { path: "/conditions", element: <ConditionsPage /> },
-      { path: "/community", element: <Community /> },
-      { path: "/therapists", element: <Therapist /> },
-      { path: "/chat", element: <Chat /> },
-      { path: "/faq", element: <FAQPage /> },
-      { path: "/terms", element: <TermsPage /> },
-      { path: "/privacy-policy", element: <PrivacyPolicyPage /> },
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/conditions",
+        element: <Conditions />,
+      },
+      {
+        path: "/community",
+        element: <Community />,
+      },
+      {
+        path: "/therapists",
+        element: <Therapists />,
+      },
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+      {
+        path: "/faq",
+        element: <FAQ />,
+      },
+      {
+        path: "/terms",
+        element: <Terms />,
+      },
+      {
+        path: "/privacy-policy",
+        element: <PrivacyPolicy />,
+      },
     ],
   },
 ]);
