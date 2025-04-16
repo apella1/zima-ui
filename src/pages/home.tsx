9f43fed8e683ec2c11b8aeccf5bb779389f82a84
John Apella <johncollons5@gmail.com>
2025-04-16 08:48:25 +0300

    fix: update about import and add prettierrc

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,12 @@
 import { PageTitle } from "@/components/page-title/page-title";
-import About from "@/components/about/about";
+import About from "@/components/about/about.tsx";
 import ConditionPreview from "@/components/condition-preview/conditions-preview";
 import FAQ from "@/components/faq/faq";
 import Header from "@/components/header/header";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-background">
+    <div className="bg-background flex min-h-screen flex-col">
       <PageTitle
         title="Mental Health Support & Resources"
         description="Access mental health resources, community support, and professional help with Zima's comprehensive platform."
